71b6d42d1afbbaa4c70eccef95352d116c2422b6
Jonathan Dahnberg <jonathan.dahnberg1@gmail.com>
2023-06-17 00:46:57 +0200

    added default font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'waterfall': "url('/images/waterfall.png')",
       },
     },
+
+    fontFamily: {
+      clash: ["ClashDisplay-Variable", "sans-serif"],
+    }
   },
   plugins: [],
-}
+};
