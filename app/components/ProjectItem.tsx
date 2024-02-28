f7de9adfffd510773ccad6d606279defe3022d98
Jonathan Meeprong Dahnberg <jonathan.dahnberg1@gmail.com>
2024-02-28 21:41:39 +0100

    Update metadata and add ProjectItem component

---
+++ app/components/ProjectItem.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { ArrowRightIcon } from '@heroicons/react/20/solid';
+
+export interface ProjectItemProps extends React.HTMLAttributes<HTMLElement> {
+  href: string;
+  label: string;
+}
+
+export const ProjectItem = ({ href, label }: ProjectItemProps) => {
+  return (
+    <div className="flex justify-between gap-x-6 py-5 group hover:cursor-pointer ">
+      <div className="flex min-w-0 gap-x-4">
+        <div className="min-w-0 flex-auto flex items-center gap-2">
+          <Link href={href} className=" text-lg leading-6 text-foreground group-hover:underline font-medium">
+            {label}
+          </Link>
+          <ArrowRightIcon className="h-4 w-4 text-foreground opacity-0 group-hover:opacity-100 transition-all group-hover:ml-2" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { Container } from '@/components/layout/Container';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import meBeer from '@/public/me_beer_3_4.jpg';
+import { Container } from '@/components/layout/Container';
 
-import Link from 'next/link';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { ProjectItem } from './components/ProjectItem';
 
 export default function Home() {
   return (
-    <main>
-      <Container className="py-20 md:py-40 min-h-screen flex flex-col gap-20 items-center md:items-start  ">
+    <main className="min-h-screen">
+      <Container className="py-20 md:py-40 flex flex-col gap-20 items-center sm:items-start">
         <h1 className="text-2xl md:text-4xl font-semibold text-primary">
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jonathan Dahnberg — a frontend developer trying to achieve
           something.
@@ -22,50 +22,27 @@ export default function Home() {
           that is based in <span className="underline">Malmö, Sweden</span> and who is nerding over fonts and colors. I
           just want everything to be easy, that is all I ask for and strive for. I find my self using the saying “Less
           is more” quite often. I mean,{' '}
-          <Link className="underline" href="#">
-            how complex can a button be?
+          <Link
+            // className="underline  decoration-primary/30 hover:decoration-primary transition"
+            className="underline"
+            href="#"
+          >
+            how complex can a button be
           </Link>
+          ?
         </h2>
       </Container>
 
       <Container className="py-20 md:py-40 flex flex-col">
-        <div className="md:ml-72">
+        <div className="md:ml-40">
           <h2 className="text-2xl md:text-4xl font-semibold text-primary">
             Some of my projects that gets past <span className="font-bold">“init”</span> and{' '}
             <span className="font-bold">“fix”</span>.
           </h2>
 
           <ul role="list" className="divide-y divide-border mt-10">
-            <li className="flex justify-between gap-x-6 py-5 group hover:cursor-pointer ">
-              <div className="flex min-w-0 gap-x-4">
-                <div className="min-w-0 flex-auto flex items-center gap-2">
-                  <Link href="#" className=" text-lg leading-6 text-foreground group-hover:underline font-medium">
-                    Buttoverse
-                  </Link>
-                  <ArrowRightIcon className="h-4 w-4 text-foreground opacity-0 group-hover:opacity-100 transition-all group-hover:ml-2" />
-                </div>
-              </div>
-              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p className=" text-sm font-medium leading-5 text-foreground">
-                  <time dateTime={new Date().toISOString()}>Aug 3, 2023</time>
-                </p>
-              </div>
-            </li>
-
-            <li className="flex justify-between gap-x-6 py-5 group hover:cursor-pointer ">
-              <div className="flex min-w-0 gap-x-4">
-                <div className="min-w-0 flex-auto flex items-center gap-2">
-                  <Link href="#" className=" text-lg leading-6 text-foreground group-hover:underline font-medium">
-                    Buttoverse
-                  </Link>
-                  <ArrowRightIcon className="h-4 w-4 text-foreground opacity-0 group-hover:opacity-100 transition-all group-hover:ml-2" />
-                </div>
-              </div>
-              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p className=" text-sm font-medium leading-5 text-foreground">
-                  <time dateTime={new Date().toISOString()}>Aug 3, 2023</time>
-                </p>
-              </div>
+            <li>
+              <ProjectItem href="#" label="Buttoverse" />
             </li>
           </ul>
         </div>
