397fe613b8f01474358c77e6c91ad34f83ed36eb
Jonathan Meeprong Dahnberg <jonathan.dahnberg1@gmail.com>
2024-03-02 18:14:10 +0100

    Refactor Container component to accept custom element type

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -1,14 +1,24 @@
 import { cn } from '@/lib/utils/cn';
-import * as React from 'react';
+import React from 'react';
 
-export interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface ContainerProps<T extends React.ElementType> {
+  as?: T;
+  className?: string;
+  children: React.ReactNode;
+}
 
-export const Container = React.forwardRef<HTMLDivElement, ContainerProps>(({ children, className, ...props }, ref) => {
-  return (
-    <div ref={ref} className={cn('mx-auto max-w-5xl px-4 sm:px-6 lg:px-8', className)} {...props}>
-      {children}
-    </div>
-  );
-});
+export const Container = React.forwardRef(
+  <T extends React.ElementType = 'div'>(
+    { as, className, children, ...props }: React.ComponentPropsWithRef<T> & ContainerProps<T>,
+    ref: React.ForwardedRef<HTMLElement>
+  ) => {
+    let Component = as ?? 'div';
 
+    return (
+      <Component {...props} ref={ref} className={cn('mx-auto max-w-5xl px-4 sm:px-6 lg:px-8', className)}>
+       {children}
+      </Component>
+    );
+  }
+);
 Container.displayName = 'Container';
