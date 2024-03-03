81b524c315806b2cc741fb3227fe65778880560c
Jonathan Meeprong Dahnberg <jonathan.dahnberg1@gmail.com>
2024-03-03 21:36:42 +0100

    Update RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
+          'min-h-dvh bg-background font-sans antialiased',
           IBMPlexMono.variable,
           LibreFranklin.variable,
         )}
@@ -44,10 +44,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className='min-h-dvh flex flex-col'>
-            <Navbar />
-            {children}
-          </div>
+          <Navbar />
+          {children}
         </ThemeProvider>
       </body>
     </html>
