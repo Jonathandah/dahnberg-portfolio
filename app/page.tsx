e529eaf466b03500f4daf319c16882cb45cc4306
Jonathan Meeprong Dahnberg <jonathan.dahnberg@skymill.io>
2024-03-11 22:52:56 +0100

    Refactor page component and update content

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,47 @@
-import Image from 'next/image'
-import Link from 'next/link'
-
-import meBeer from '@/public/me_beer_3_4.jpg'
-import { Container } from '@/components/layout/Container'
-
-// import { ProjectItem } from './components/ProjectItem'
 import { FadeIn } from '@/components/display/FadeIn'
+import { Indent } from '@/components/display/Indent'
+import { Container } from '@/components/layout/Container'
+import meBeer from '@/public/me_peace.jpg'
+import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
   return (
     <main>
       <FadeIn>
-        <Container className="flex flex-col items-center gap-20 py-24 sm:items-start sm:py-32">
+        <Container className="flex flex-col gap-20 py-24  sm:py-32">
           <h1 className="text-3xl font-semibold text-primary sm:text-4xl">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jonathan Dahnberg —
-            a frontend developer trying to achieve something.
+            <Indent />
+            Jonathan Meeprong Dahnberg — a&nbsp;
+            <span className="underline">frontend developer</span> trying to
+            achieve stuff.
           </h1>
 
           <Image
-            className=" h-auto w-56 grayscale md:w-72"
+            className="w-56 h-auto grayscale md:w-72"
             src={meBeer}
+            priority
             alt="Me drinking beer"
-          />      
+          />
 
-          <h2 className=" text-3xl font-semibold text-primary sm:text-4xl">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Who am I? — Sorry to
-            disappoint you, I’m just a regular guy that is based in{' '}
-            <span className="underline">Malmö, Sweden</span> and who is nerding
-            over fonts and colors. I just want everything to be easy, that is
-            all I ask for and strive for. I find my self using the saying “Less
-            is more” quite often. I mean,{' '}
-            <Link className="underline" href="#">
-              how complex can a button be
-            </Link>
-            ?
+          <h2 className="text-3xl font-semibold text-primary sm:text-4xl">
+            <Indent />
+            Who am I? — Sorry to disappoint yo. I’m just a regular guy that is
+            based in <span className="underline">Malmö, Sweden</span> and who is
+            nerding over fonts and colors. I mean who doesn’t like a good old
+            Libre Franklin? Ammarite?
           </h2>
         </Container>
       </FadeIn>
-      {/* <Container className="mt-16 sm:mt-20 flex flex-col">
+      {/* <Container className="flex flex-col mt-16 sm:mt-20">
         <div className="md:ml-40">
           <FadeIn>
-            <h2 className="text-3xl sm:text-4xl font-semibold text-primary">
+            <h2 className="text-3xl font-semibold sm:text-4xl text-primary">
               Some of my projects that gets past <span className="font-bold">“init”</span> and{' '}
               <span className="font-bold">“fix”</span>.
             </h2>
           </FadeIn>
 
-          <ul role="list" className="divide-y divide-border mt-10">
+          <ul role="list" className="mt-10 divide-y divide-border">
             <FadeIn>
               <li>
                 <ProjectItem href="#" label="Buttoverse" />
