72c1857b96109932f6667343021ed98931bdc9db
Jonathan Meeprong Dahnberg <jonathan.dahnberg@skymill.io>
2024-03-11 22:53:18 +0100

    Fix import order in button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react'
+import { cn } from '@/lib/utils/cn'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
-import { cn } from '@/lib/utils/cn'
+import * as React from 'react'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        text: 'text-primary',
       },
       size: {
         'no-padding': 'p-0',
