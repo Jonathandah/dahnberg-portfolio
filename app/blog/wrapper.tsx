304194509df9684b61b068241c3ae006ef992e61
Jonathan Meeprong Dahnberg <jonathan.dahnberg@skymill.io>
2024-03-21 19:43:47 +0100

    Add custom SVG icon for BreadcrumbSeparator

--- app/blog/wrapper.tsx
+++ app/blog/wrapper.tsx
@@ -64,7 +64,22 @@ export default function BlogArticleWrapper({
                       <Link href="/blog">Blog</Link>
                     </BreadcrumbLink>
                   </BreadcrumbItem>
-                  <BreadcrumbSeparator>/</BreadcrumbSeparator>
+                  <BreadcrumbSeparator>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="lucide lucide-slash"
+                    >
+                      <path d="M22 2 2 22" />
+                    </svg>
+                  </BreadcrumbSeparator>
                   <BreadcrumbItem>
                     <BreadcrumbPage>{article.title}</BreadcrumbPage>
                   </BreadcrumbItem>
