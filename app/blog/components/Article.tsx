7c6cbdcfe6d372033f663367304a7b275669117a
Jonathan Meeprong Dahnberg <jonathan.dahnberg@skymill.io>
2024-03-23 00:21:09 +0100

    Refactor Article component to improve readability and add Eyebrow component

--- app/blog/components/Article.tsx
+++ app/blog/components/Article.tsx
@@ -10,19 +10,28 @@ export interface ArticleProps {
 export function Article({ article }: ArticleProps) {
   return (
     <article className="flex relative justify-between gap-6">
-      <div className="inline-flex items-center">
-        <h2 className="text-lg font-semibold tracking-tight text-foreground line-clamp-1">
-          <Link className=" hover:underline" href={`${article.href}`}>
-            <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
-            <span className="relative z-10">{article.title}</span>
-          </Link>
-        </h2>
-      </div>
+      <div className="group max-w-sm">
+        <div className=" inline-flex gap-2">
+          <h2 className="font-bold transition  text-muted-foreground group-hover:text-foreground line-clamp-1">
+            <Link href={`${article.href}`}>
+              <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
+              <span className="relative z-10">{article.title}</span>
+            </Link>
+          </h2>
+          <Eyebrow
+            className="!text-xs font-thin"
+            as="time"
+            dateTime={article.datetime}
+          >
+            {/* TODO: use formatDate when you have time to fix it in mobile */}
+            {formatDate(new Date(article.datetime))}
+          </Eyebrow>
+        </div>
 
-      <Eyebrow as="time" dateTime={article.datetime}>
-        {/* TODO: use formatDate when you have time to fix it in mobile */}
-        {formatDate(new Date(article.datetime))}
-      </Eyebrow>
+        <p className=" font-light text-muted-foreground transition group-hover:text-foreground ">
+          {article.description}
+        </p>
+      </div>
     </article>
   )
 }
