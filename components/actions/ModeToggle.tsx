6cd443989dbabfae2e5fcc2a7158042bfbfaa621
Jonathan Meeprong Dahnberg <jonathan.dahnberg@skymill.io>
2024-03-23 00:09:44 +0100

    Refactor ModeToggle component to include ButtonProps

--- components/actions/ModeToggle.tsx
+++ components/actions/ModeToggle.tsx
@@ -1,20 +1,24 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
+import { Button, ButtonProps } from '@/components/ui/button'
+import { cn } from '@/lib/utils/cn'
 import { useTheme } from 'next-themes'
 import * as React from 'react'
 
-export function ModeToggle() {
+export interface ModeToggleProps extends ButtonProps {}
+
+export function ModeToggle({ className, ...props }: ModeToggleProps) {
   const { setTheme, theme } = useTheme()
 
   return (
     <Button
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       variant="text"
-      className="uppercase text-xs font-semibold"
+      className={cn('uppercase text-xs text-muted-foreground', className)}
+      {...props}
     >
-      <span className="block transition-all dark:hidden">Light -</span>
-      <span className="transition-all hidden dark:block">Dark +</span>
+      <span className="block  dark:hidden">Light -</span>
+      <span className=" hidden dark:block">Dark +</span>
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
