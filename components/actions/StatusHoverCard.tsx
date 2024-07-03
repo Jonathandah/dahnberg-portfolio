87a6c64ec7798ad7da6518a20fa0df14a81edd42
Jonathan Meeprong Dahnberg <43029553+Jonathandah@users.noreply.github.com>
2024-07-03 21:17:00 +0200

    Feature/status message (#20)
    * started on status feature
    
    * added stuff
    
    ---------
    
    Co-authored-by: Jonathan Meeprong Dahnberg <jonathan.dahnberg@skymill.io>

---
+++ components/actions/StatusHoverCard.tsx
@@ -0,0 +1,40 @@
+import { Button } from '../ui/button'
+import { Eyebrow } from '@/app/blog/_components/Eyebrow'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card'
+import { formatDate } from '@/lib/utils/formatDate'
+
+let status = {
+  text: 'On vacation, will be back July 15th :)',
+  datetime: '2024-07-03',
+}
+
+export function StatusHoverCard() {
+  return (
+    <HoverCard openDelay={100}>
+      <HoverCardTrigger asChild>
+        <Button
+          className="text-xs items-start uppercase cursor-default"
+          variant="text"
+        >
+          _Status
+        </Button>
+      </HoverCardTrigger>
+      <HoverCardContent className="bg-transparent/[0.03] dark:bg-transparent/15 shadow-none border-none w-fit max-w-80 normal-case">
+        <div className="flex flex-col">
+          <p className="text-sm font-serif">{status.text}</p>
+          <Eyebrow
+            className="text-xs font-sans p-0"
+            as="time"
+            dateTime={status.datetime}
+          >
+            {formatDate(new Date(status.datetime))}
+          </Eyebrow>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  )
+}
--- components/layout/HomeNavbar.tsx
+++ components/layout/HomeNavbar.tsx
@@ -1,19 +1,19 @@
 import { ModeToggle } from '../actions/ModeToggle'
+import { StatusHoverCard } from '../actions/StatusHoverCard'
 import { FadeIn, FadeInStagger } from '../display/FadeIn'
 import { Button } from '../ui/button'
 import { Navbar } from './Navbar'
-import { cn } from '@/lib/utils/cn'
 import { ArrowUpRightIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 
 export function HomeNavbar() {
   return (
-    <Navbar>
+    <Navbar className="uppercase">
       <FadeInStagger className="relative flex flex-col sm:flex-row items-start justify-between gap-2 h-fit">
         <div className="flex items-start w-full">
           <FadeIn>
             <Button asChild size="no-padding" variant="text">
-              <Link className="text-xs uppercase pr-4 py-2 block" href="/">
+              <Link className="text-xs  pr-4 py-2 block" href="/">
                 Dahnberg
                 <br />
                 Portfolio
@@ -22,7 +22,7 @@ export function HomeNavbar() {
           </FadeIn>
 
           <FadeIn>
-            <div className="flex flex-col items-start uppercase  px-4 py-2 ">
+            <div className="flex flex-col items-start   px-4 py-2 ">
               <Button
                 className="text-xs items-start"
                 asChild
@@ -56,15 +56,17 @@ export function HomeNavbar() {
           </FadeIn>
 
           <FadeIn>
+            <StatusHoverCard />
+          </FadeIn>
+          
+          <FadeIn>
             <ModeToggle />
           </FadeIn>
 
+      
+
           <FadeIn className="ml-auto">
-            <Button
-              asChild
-              variant="link"
-              className={cn('text-xs uppercase pr-0 pl-0')}
-            >
+            <Button asChild variant="link" className="text-xs  pr-0 pl-0">
               <Link href="/blog">Blog</Link>
             </Button>
           </FadeIn>
